34d60cb9f166c934526eeae4b5918b681dde3320
mikael <mikael.quadros@gmail.com>
2023-12-03 15:28:15 -0300

    ajustes firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:chat_flutter/pages/DashboardPage/dashboard_page.dart';
 import 'package:chat_flutter/pages/LoginPage/login_page.dart';
 import 'package:chat_flutter/pages/RegisterPage/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
