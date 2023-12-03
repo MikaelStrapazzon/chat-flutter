07813bc74309dea636f33b4656ad1e0e5e9592ce
mikael <mikael.quadros@gmail.com>
2023-12-03 18:41:09 -0300

    Registro no firebase

--- lib/pages/RegisterPage/register_page.dart
+++ lib/pages/RegisterPage/register_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../services/fire_auth.dart';
+import '../DashboardPage/dashboard_page.dart';
 import '../LoginPage/login_page.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -8,13 +10,26 @@ class RegisterPage extends StatelessWidget {
   static String id = '/RegisterPage';
 
   final formKey = GlobalKey<FormState>();
-  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     Color primaryColor = Theme.of(context).colorScheme.primaryContainer;
     Color secondaryColor = Theme.of(context).colorScheme.secondaryContainer;
+
+    void register() async {
+      try {
+        await FireAuth.registerUsingEmailPassword(
+          email: emailController.text,
+          password: passwordController.text,
+        ).then((value) => Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const DashboardPage())));
+      } on Exception catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(backgroundColor: Colors.red, content: Text(e.toString())));
+      }
+    }
 
     return Scaffold(
       backgroundColor: Colors.transparent,
@@ -48,16 +63,24 @@ class RegisterPage extends StatelessWidget {
                   ),
                   const SizedBox(height: 8.0),
                   TextFormField(
-                    controller: nameController,
+                    controller: emailController,
                     decoration: const InputDecoration(
-                      labelText: 'Name',
+                      labelText: 'Email',
                       errorStyle: TextStyle(color: Colors.red),
                     ),
                     autovalidateMode: AutovalidateMode.onUserInteraction,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
-                        return 'Please enter a name';
+                        return 'Please enter a email';
                       }
+
+                      final emailRegex = RegExp(
+                          r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+
+                      if (!emailRegex.hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+
                       return null;
                     },
                   ),
@@ -109,6 +132,8 @@ class RegisterPage extends StatelessWidget {
                       if (!formKey.currentState!.validate()) {
                         return;
                       }
+
+                      register();
                     },
                   ),
                 ],
--- lib/services/fire_auth.dart
+++ lib/services/fire_auth.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 class FireAuth {
   // For registering a new user
   static Future<User?> registerUsingEmailPassword({
-    required String name,
     required String email,
     required String password,
   }) async {
@@ -11,23 +10,14 @@ class FireAuth {
     User? user;
 
     try {
-      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+      await auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
 
-      user = userCredential.user;
-      await user!.updateProfile(displayName: name);
-      await user.reload();
       user = auth.currentUser;
     } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        print('The password provided is too weak.');
-      } else if (e.code == 'email-already-in-use') {
-        print('The account already exists for that email.');
-      }
-    } catch (e) {
-      print(e);
+      throw Exception(e.message);
     }
 
     return user;
