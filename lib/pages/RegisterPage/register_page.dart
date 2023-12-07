2c369ae76817893437c2c7e2088fcf67663945f9
mikael <mikael.quadros@gmail.com>
2023-12-07 11:09:10 -0300

    Ajuste tela de registro

--- lib/pages/RegisterPage/register_page.dart
+++ lib/pages/RegisterPage/register_page.dart
@@ -32,141 +32,144 @@ class RegisterPage extends StatelessWidget {
     }
 
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-          colors: [primaryColor, secondaryColor],
-          stops: const [0.1, 1],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        )),
-        padding: const EdgeInsets.all(14.0),
-        child: Column(children: [
-          Expanded(
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Image(
-                    image: AssetImage('assets/logos/logo.png'),
-                    width: 90,
-                  ),
-                  const Text(
-                    'Register Form',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 6.0),
-                  TextFormField(
-                    controller: nameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Name',
-                      errorStyle: TextStyle(color: Colors.red),
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            constraints: const BoxConstraints(minHeight: 600),
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              colors: [primaryColor, secondaryColor],
+              stops: const [0.1, 1],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            )),
+            padding: const EdgeInsets.all(14.0),
+            child: Column(children: [
+              Expanded(
+                  child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/logos/logo.png'),
+                      width: 90,
                     ),
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a name';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 6.0),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      errorStyle: TextStyle(color: Colors.red),
+                    const Text(
+                      'Register Form',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
                     ),
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a email';
-                      }
+                    const SizedBox(height: 8.0),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Name',
+                        errorStyle: TextStyle(color: Colors.red),
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a name';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 8.0),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        errorStyle: TextStyle(color: Colors.red),
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a email';
+                        }
 
-                      final emailRegex = RegExp(
-                          r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
+                        final emailRegex = RegExp(
+                            r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
 
-                      if (!emailRegex.hasMatch(value)) {
-                        return 'Please enter a valid email address';
-                      }
+                        if (!emailRegex.hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
 
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 6.0),
-                  TextFormField(
-                    controller: passwordController,
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      errorStyle: TextStyle(color: Colors.red),
+                        return null;
+                      },
                     ),
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a password';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 6.0),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: 'Confirm Password',
-                      errorStyle: TextStyle(color: Colors.red),
+                    const SizedBox(height: 8.0),
+                    TextFormField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        errorStyle: TextStyle(color: Colors.red),
+                      ),
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter a password';
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value == null || value != passwordController.text) {
-                        return 'Please enter the same password';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 12.0),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
+                    const SizedBox(height: 8.0),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm Password',
+                        errorStyle: TextStyle(color: Colors.red),
+                      ),
+                      validator: (value) {
+                        if (value == null || value != passwordController.text) {
+                          return 'Please enter the same password';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
                         ),
                       ),
-                    ),
-                    child: const Text(
-                      'Register',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      if (!formKey.currentState!.validate()) {
-                        return;
-                      }
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        if (!formKey.currentState!.validate()) {
+                          return;
+                        }
 
-                      register();
-                    },
-                  ),
-                ],
-              ),
-            ),
+                        register();
+                      },
+                    ),
+                  ],
+                ),
+              )),
+              GestureDetector(
+                child: const Text(
+                  "Back to login",
+                  style: TextStyle(fontSize: 14),
+                  textAlign: TextAlign.center,
+                ),
+                onTap: () {
+                  Get.off(() => LoginPage());
+                },
+              )
+            ]),
           ),
-          GestureDetector(
-            child: const Text(
-              "Back to login",
-              style: TextStyle(fontSize: 14),
-              textAlign: TextAlign.center,
-            ),
-            onTap: () {
-              Get.off(() => LoginPage());
-            },
-          )
-        ]),
-      ),
-    );
+        ));
   }
 }
